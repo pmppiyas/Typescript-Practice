d392070893aa77b4a2ff924efc01f854f38a8f8e
PmpPiyas <princemahmudpiyas@gmail.com>
2025-04-28 00:16:45 +0600

    Any type

---
+++ output/dynamicType.js
@@ -0,0 +1,12 @@
+"use strict";
+let a = [];
+a.push("Hello", 10);
+console.log(a);
+let student;
+student = {
+    name: "Piyas",
+    class: "five",
+    roll: 3,
+    age: 20,
+};
+console.log(student);
---
+++ src/dynamicType.ts
@@ -0,0 +1,19 @@
+let a: any[] = [];
+
+a.push("Hello", 10);
+console.log(a);
+
+let student: {
+  name: any;
+  class: any;
+  roll: any;
+  age: any;
+};
+
+student = {
+  name: "Piyas",
+  class: "five",
+  roll: 3,
+  age: 20,
+};
+console.log(student);
