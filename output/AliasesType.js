ecb0da318c909bad801e95e933c35bddc8d98a4a
PmpPiyas <princemahmudpiyas@gmail.com>
2025-04-29 00:08:00 +0600

    Type Aliases

---
+++ output/AliasesType.js
@@ -0,0 +1,8 @@
+"use strict";
+const studentData = {
+    name: "Piyas",
+    age: 20,
+    class: "10th",
+    school: "ABC High School",
+};
+console.log(studentData);
---
+++ src/AliasesType.ts
@@ -0,0 +1,14 @@
+type StudentData = {
+  name: string;
+  age: number;
+  class: string;
+  school: string;
+};
+
+const studentData: StudentData = {
+  name: "Piyas",
+  age: 20,
+  class: "10th",
+  school: "ABC High School",
+};
+console.log(studentData);
